f5b8ce73d0e39d80e659fe31b12c9d90cbd92b7f
Thorvald Ballestad <thorvalb@ntnu.no>
2020-12-15 15:29:37 +0100

    Moves some text to top of index

--- index.php
+++ index.php
@@ -6,6 +6,16 @@
 	        <img src="assets/sample_image.jpg" class="w3-card" style="margin-top: -135px;">
 	    </div>
         </div>
+
+
+        <section class="w3-container narrow w3-panel w3-margin-top w3-sand w3-leftbar w3-padding-32">
+                 <h2>Fysikklab i IFYKJG1000/IFYKJG1001</h2>
+                 <p>
+                      På denne siden finner du alt av praktisk informasjon og ressurser som du trenger til fysikklab.
+Her kan man ha en velkomsttekst.
+</p>
+                                                                                                                    <i class="material-icons w3-xxlarge">announcement</i> Gjør alle forberedelsene før du kommer på lab!
+        </section>
         <div class="flex-container flex-center">
             <div class="badge">
                 <a href="forberedelse.phtml" class="badge__circle shadow">
@@ -29,18 +39,4 @@
             </div>
         </div><!-- Flex container -->
 
-
-        <section class="w3-container w3-panel w3-sand w3-margin w3-padding-32">
-                 <h2>Fysikklab i IFYKJG1000/IFYKJG1001</h2>
-                 <p>
-                      På denne siden finner du alt av praktisk informasjon og ressurser som du trenger til fysikklab.
-</p>
-                                                                                                                    <i class="material-icons w3-xxlarge">announcement</i> Gjør alle forberedelsene før du kommer på lab!
-        </section>
-
-
-        <section class="w3-container w3-panel w3-sand w3-margin w3-padding-32">
-            <h2>Lorem ipsum dolor sit amet</h2>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque vitae justo facilisis, elementum arcu nec, posuere diam. Phasellus et sodales urna. Nulla lacinia elit tortor, sed blandit mauris pharetra euismod. Nulla diam tellus, commodo eu congue ac, molestie id nulla. Nulla at lorem sit amet neque hendrerit faucibus at non nibh. Etiam iaculis scelerisque orci, id mollis est pretium a. Proin dictum fermentum quam, eget ullamcorper sapien. Fusce elementum est nibh, eu dignissim lacus euismod vitae. Ut id arcu augue. Pellentesque tempus iaculis consectetur. Proin accumsan purus non ipsum iaculis, eget scelerisque eros aliquet. Integer cursus mauris a fermentum imperdiet. Pellentesque odio leo, tempus id orci sit amet, dapibus commodo ligula. Nunc fermentum sem risus, vitae dictum lacus vulputate a. Curabitur sit amet sodales arcu. Nunc venenatis dignissim dolor eu consectetur.</p>
-        </section>
 <?php include 'footer.phtml'; ?>
